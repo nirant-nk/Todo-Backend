ed1e2e5ede2dc8fa060f26a991de7d678a3c3de7
nirant-nk <kalenirant@gmail.com>
2024-12-19 17:18:56 +0530

    Auth Middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import ApiError from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const authorizeAccess = asyncHandler(async (req,res,next) => {
+    try {
+        const token = await 
+        req.cookies?.accessToken || req.header("Authorization").replace("Bearer ","")
+
+        if(!token) throw new ApiError(404,"No token found, Unauthorized Access.")
+
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select(
+            "-password -refreshToken"
+        )
+
+        if(!user) throw new ApiError(400,"Invalid Token! No such user!")
+
+        req.user = user
+        next()
+    } catch (error) {
+        res.status(400).json(
+            new ApiError(400,`State - Authorization\nServer side error: ${error}`)
+        );
+    }
+})
+
+export default authorizeAccess
